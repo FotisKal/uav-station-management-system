258aac1e73547e8b85e9c7ffdb31c867c28314cb
fotis <fotis_kalokiris@outlook.com>
2023-03-24 04:42:37 -0400

    added relationship with RolePermission and toList Method into Role

--- app/Uavsms/UserRole/Role.php
+++ app/Uavsms/UserRole/Role.php
@@ -7,4 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     protected $guarded = ['id', 'created', 'updated_at'];
+
+    /**
+     * Has Many Permissions
+     */
+    public function permissions()
+    {
+        return $this->hasMany('App\Uavsms\UserRole\RolePermission');
+    }
+
+    /**
+     * Role Permissions To List
+     */
+    public function permissionsToList()
+    {
+        $data = [];
+
+        $permissions = $this->permissions;
+
+        if (count($permissions) == 0) {
+            return $data;
+        }
+
+        foreach ($permissions as $model) {
+            $data[$model->id] = $model->permission;
+        }
+
+        return $data;
+    }
 }
