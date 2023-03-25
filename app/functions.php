ce7a6ec8032749af57d08a28bc135f963eeb4c2d
fotis <fotis_kalokiris@outlook.com>
2023-03-25 12:33:56 -0400

    added a function into functions.php, which shows performance stats

--- app/functions.php
+++ app/functions.php
@@ -7,3 +7,15 @@
     | This file contains the helper functions.
     |
     */
+
+/**
+ * Show performance Stats
+ */
+function show_stats() {
+    $ret = 'Time ellapsed: ' . round(microtime(true) - LARAVEL_START, 3) . ' s' . chr(10);
+    $ret .= 'Memory get usage: ' . round(memory_get_usage(true) / 1000000, 3) . ' MB' . chr(10);
+//    $ret .= 'Memory usage pid: ' . memory_usage() . chr(10);
+    $ret .= 'Memory get peak usage: ' . round(memory_get_peak_usage(true) / 1000000, 3) . ' MB' . chr(10);
+
+    return $ret;
+}
