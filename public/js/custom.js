629f845ec53958e3d0957282fce07bf127f5b97c
fotis <fotis_kalokiris@outlook.com>
2023-03-27 08:25:24 -0400

    changed sidebar's blade App Logo

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,6 @@
-$(document).on("click","ul.nav li.parent > a ", function(){          
-    $(this).find('i').toggleClass("fa-minus");      
-}); 
+$(document).on("click","ul.nav li.parent > a ", function(){
+    $(this).find('i').toggleClass("fa-minus");
+});
 $(".sidebar span.icon").find('em:first').addClass("fa-plus");
 
 $('#calendar').datepicker({
@@ -10,3 +10,13 @@ $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
+
+$(document).ready(function(){
+    setInterval(function(){
+        opt_1 = 'fa fa-battery-full';
+        opt_2 = 'fa fa-battery-three-quarters';
+        let sidebar_icon = $("#sidebar-em");
+
+        (sidebar_icon.attr("class") === opt_1) ? sidebar_icon.attr("class", opt_2) : sidebar_icon.attr("class", opt_1);
+    }, 1000);
+});
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <nav class="sidebar col-xs-12 col-sm-4 col-lg-3 col-xl-2">
     <h1 class="site-title">
-        <a href="index.html"><em class="fa fa-rocket"></em> {{ __('UAV Stations') }} </a>
+        <a href="{{ url('/dashboard') }}"><em class="fa fa-battery-full" id="sidebar-em"></em> {{ __('UAV Stations') }} </a>
     </h1>
 
     <a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><em class="fa fa-bars"></em></a>
