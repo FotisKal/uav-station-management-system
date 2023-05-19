721e137d8d1d55a48d4d4820f3334de54c21c674
fotis <fotis_kalokiris@outlook.com>
2023-05-19 12:08:18 -0400

    created view blade for Uavs (with nav)

---
+++ resources/views/uavs/partials/nav.blade.php
@@ -0,0 +1,17 @@
+<div class="card-header">
+    <ul class="nav nav-tabs card-header-tabs">
+        <li class="nav-item">
+            <a class="nav-link {{ $selected_nav == 'view' ? 'active' : '' }}"
+            href="{{ url('/uavs/' . $uav->id . '/view') }}">
+                {{ __('View') }}
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ $selected_nav == 'edit' ? 'active' : '' }}
+            {{ !Auth::user()->hasPermission(\App\Uavsms\UserRole\Permission::CAN_MANAGE_UAVS) ? 'disabled' : '' }}"
+            href="{{ !Auth::user()->hasPermission(\App\Uavsms\UserRole\Permission::CAN_MANAGE_UAVS) ? '#' : url('/uavs/' . $uav->id . '/edit') }}">
+                {{ __('Edit') }}
+            </a>
+        </li>
+    </ul>
+</div>
---
+++ resources/views/uavs/view.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="col-lg-6 mb-4">
+        <div class="row card text-center bg-default">
+
+            @include('uavs.partials.nav')
+
+            <div class="card-block">
+                <div class="table-responsive table-hover table-bordered">
+                    <table class="table table-striped">
+                        <tbody>
+                            <tr>
+                                <th class="text-left"> {{ __('Name') }} </th>
+                                <td> {{ $uav->name }} </td>
+                            </tr>
+                            <tr>
+                                <th class="text-left"> {{ __('UAV Owner\'s Email') }} </th>
+                                <td> {{ $uav->user->email }} </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <br>
+                @if (Auth::user()->hasPermission(\App\Uavsms\UserRole\Permission::CAN_MANAGE_UAVS))
+                    <a href="{{ url('/uavs/' . $uav->id . '/edit') }}" class="btn btn-md btn-primary float-left"> {{ __('Edit') }} </a>
+                @endif
+            </div>
+        </div>
+    </div>
+
+@endsection
