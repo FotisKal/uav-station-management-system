084b4b754211ed536c42ddde1d99cf38e43eaf96
fotis <fotis_kalokiris@outlook.com>
2023-09-21 14:47:34 -0400

    enabled soft deletes for User Model

--- app/User.php
+++ app/User.php
@@ -8,10 +8,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Validation\Rule;
 use Validator;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
